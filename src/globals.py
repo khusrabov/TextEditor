565c62df8e71c3b5d89da019b69de90b3302e191
Rustam Khusrabov <khusrabov.re@MacBook-Air-Rustam.local>
2023-04-08 23:07:01 +0300

    added globals.py

---
+++ src/globals.py
@@ -0,0 +1,19 @@
+# view colors
+
+view_colors = {'dark': {
+    'text_bg': 'black',
+    'text_fg': 'lime',
+    'cursor': 'red',
+    'select_bg': '#ADD8E6',
+    }, 'light': {
+    'text_bg': 'white',
+    'text_fg': 'black',
+    'cursor': 'red',
+    'select_bg': '#ADD8E6',
+    }}
+
+# fonts
+
+fonts = {'Arial': {'font': 'Arial 12 bold'},
+         'CSM': {'font': ('Comic Sans MS', 11, 'bold')},
+         'TNR': {'font': ('Times New Roman', 12, 'bold')}}
